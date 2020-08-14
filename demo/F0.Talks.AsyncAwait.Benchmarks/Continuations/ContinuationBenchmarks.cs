7ad265fe7606e363f8abf40b45b6b59b00ca1f32
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-08-15 00:13:33 +0200

    add Benchmarks
    measure the performance impact of redundant await

---
+++ demo/F0.Talks.AsyncAwait.Benchmarks/Continuations/ContinuationBenchmarks.cs
@@ -0,0 +1,64 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using System.Threading.Tasks;
+
+namespace F0.Talks.AsyncAwait.Benchmarks.Continuations
+{
+    [MemoryDiagnoser]
+    [Config(typeof(ContinuationConfig))]
+    public class ContinuationBenchmarks
+    {
+        [Benchmark]
+        public Task<int> CompletedTask_Synchronous()
+        {
+            int number = 42;
+
+            return GenerateSynchronously(number);
+        }
+
+        [Benchmark]
+        public async Task<int> CompletedTask_UnneccessaryAsync()
+        {
+            int number = 42;
+
+            return await GenerateSynchronously(number);
+        }
+
+        [Benchmark]
+        public Task<int> IncompleteTask_Synchronous()
+        {
+            int number = 42;
+
+            return GenerateAsynchronously(number);
+        }
+
+        [Benchmark]
+        public async Task<int> IncompleteTask_UnneccessaryAsync()
+        {
+            int number = 42;
+
+            return await GenerateAsynchronously(number);
+        }
+
+        private static Task<int> GenerateSynchronously(int number)
+        {
+            return Task.FromResult(number);
+        }
+
+        private static async Task<int> GenerateAsynchronously(int number)
+        {
+            await Task.Yield();
+            return number;
+        }
+
+        private class ContinuationConfig : ManualConfig
+        {
+            public ContinuationConfig()
+            {
+                AddColumn(new TagColumn("Scenario", name => name.Split('_')[0]));
+                AddColumn(new TagColumn("Usage", name => name.Split('_')[1]));
+            }
+        }
+    }
+}
---
+++ demo/F0.Talks.AsyncAwait.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System.Diagnostics;
+
+namespace F0.Talks.AsyncAwait.Benchmarks
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            IConfig? config = Debugger.IsAttached
+                ? new DebugInProcessConfig()
+                : DefaultConfig.Instance;
+
+            _ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+        }
+    }
+}
